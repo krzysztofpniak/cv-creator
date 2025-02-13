708f9573a654a2405b75756502c15ba1bad52016
Krzysztof Pniak <krzysztof.pniak@gmail.com>
2025-02-13 15:32:40 +0100

    feat(ready)

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,178 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import ReactMarkdown from "react-markdown";
+import "./App.css";
+import data from "./data";
+
+const mainColor = "rgb(0, 117, 167)";
+
+const SectionPlain = ({ title, children }) => (
+  <section>
+    {title && <h2 style={{ fontSize: "14pt", margin: 0 }}>{title}</h2>}
+    <p style={{ fontSize: "9pt", margin: "2mm 0" }}>{children}</p>
+  </section>
+);
+
+const SectionHeader = ({ title }) => (
+  <h2
+    style={{
+      borderTop: "1px solid lightgrey",
+      borderBottom: "1px solid lightgrey",
+      fontSize: "14pt",
+      margin: 0,
+      color: mainColor,
+    }}
+  >
+    {title}
+  </h2>
+);
+
+const List = ({ items }) => (
+  <ul style={{ margin: 0, fontSize: "9pt" }}>
+    {items.map((dot, idx) => (
+      <li key={idx}>
+        <ReactMarkdown>{dot}</ReactMarkdown>
+      </li>
+    ))}
+  </ul>
+);
+
+const SectionWorkItem = ({ workItem }) => (
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      gap: "1mm",
+      padding: "2mm 0",
+    }}
+  >
+    <h3 style={{ margin: 0, fontSize: "13pt" }}>{workItem.title}</h3>
+    {workItem.company && workItem.dates && (
+      <h4
+        style={{
+          margin: "0",
+          fontSize: "11pt",
+          fontStyle: "italic",
+          fontWeight: "normal",
+        }}
+      >
+        {`${workItem.company} | ${workItem.dates}`}
+      </h4>
+    )}
+    {workItem.dots && <List items={workItem.dots} />}
+  </div>
+);
+
+const SectionWork = ({ title, items }) => (
+  <section>
+    <SectionHeader title={title} />
+    {items.map((item) => (
+      <SectionWorkItem workItem={item} />
+    ))}
+  </section>
+);
+
+const SectionSkills = ({ items }) => (
+  <section
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      gap: "1mm",
+      padding: "2mm 0",
+    }}
+  >
+    <SectionHeader title="Technical Skills" />
+    <List items={items} />
+  </section>
+);
+
+const SideHeader = ({ title }) => (
+  <h3
+    style={{
+      background: "red",
+      margin: "3mm -5mm",
+      padding: "2mm 5mm",
+      background: "#0003",
+    }}
+  >
+    {title}
+  </h3>
+);
+
+const InfoRow = ({ title, value, valueSize }) => (
+  <div>
+    <strong style={{ fontSize: "11pt" }}>{title}</strong>
+    <div style={{ fontSize: valueSize ?? "11pt" }}>{value}</div>
+  </div>
+);
+
+const SideList = ({ items }) => (
+  <ul style={{ padding: 0, listStyle: "none", margin: 0 }}>
+    {items.map((item, idx) => (
+      <li key={idx}>{item}</li>
+    ))}
+  </ul>
+);
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="igor">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        style={{
+          fontSize: "12pt",
+          background: mainColor,
+          color: "white",
+          width: "55mm",
+          padding: "5mm",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <h1 style={{ fontSize: "28pt", margin: 0 }}>Krzysztof Pniak</h1>
+        <h2 style={{ fontSize: "14pt" }}>Senior Developer</h2>
+        <img src="/profile.png" style={{ width: "100%" }} />
+        <SideHeader title="Personal Info" />
+        <div style={{ display: "flex", flexDirection: "column", gap: "2mm" }}>
+          <InfoRow title="Email" value="krzysztof.pniak@gmail.com" />
+          <InfoRow title="Phone" value="+48 506 803 802" />
+          <InfoRow
+            title="Linkedin"
+            value="https://www.linkedin.com/in/krzysztofpniak"
+            valueSize="8pt"
+          />
+          <InfoRow
+            title="Github"
+            value="https://github.com/krzysztofpniak"
+            valueSize="8pt"
+          />
+        </div>
+        <SideHeader title="Languages" />
+        <SideList items={data.languages} />
+        <SideHeader title="Hobbies" />
+        <SideList items={data.hobbies} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div
+        style={{
+          background: "white",
+          flex: 1,
+          padding: "5mm",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <SectionPlain>{data.header}</SectionPlain>
+        <SectionWork title="Work History" items={data.work} />
+        <SectionSkills items={data.skills} />
+        <SectionWork title="Education" items={data.education} />
+        <div style={{ flex: 1 }} />
+        <footer style={{ fontSize: "6.5pt", fontStretch: "extra-condensed" }}>
+          I hereby consent to my personal data being processed by Onsights for
+          the purpose of considering my application for the advertised vacancy.
+        </footer>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,57 @@
+export default {
+  header:
+    "Experienced software engineer with a strong background in JavaScript, Node.js, and cloud-native development. Passionate about building scalable, high-performance applications and delivering impactful solutions to enterprise clients. Adept at working in agile environments and collaborating with cross-functional teams to drive project success.",
+  work: [
+    {
+      title: "Solution Architect",
+      company: "Elisa Polystar",
+      dates: "2016-07 – 2024-10",
+      dots: [
+        "Designed and built scalable cloud-native(**AWS**) **big data** application using **Node.js**, **Spark**, **Scala** and **React.js**.",
+        "Implemented **Spark SQL** syntax validation using **ANTLR** for enhanced query analysis.",
+        "Integrated **Monaco Editor** with a custom Language Server Protocol (**LSP**) for improved development experience.",
+        "Developed and maintained robust APIs using **GraphQL** and **REST.** in both **Node.js** and **Java.**",
+        "Assisted in defining structured development practices, including source code management, **CI/CD**, and deployment strategies.",
+        "Implemented automated testing solutions, including unit, integration, and end-to-end testing.",
+        "Led code reviews and provided mentorship to junior developers.",
+        "Worked closely with clients to translate requirements into functional solutions.",
+        "Implemented observability using **Grafana, Prometheus, Loki**, and **Tempo** to enhance monitoring, logging, and tracing across distributed systems",
+      ],
+    },
+    {
+      title: "Lead Software Engineer",
+      company: "Online Solutions",
+      dates: "2011-01 – 2016-07",
+      dots: [
+        "Led a five-person development team to implement **CRM** and **SharePoint** solutions for various clients.",
+        "Managed data migration between **ERP** systems, ensuring accuracy and consistency.",
+        "Collaborated closely with clients to gather and understand business requirements.",
+        "Coordinated technical aspects to facilitate seamless integration across platforms.",
+      ],
+    },
+    {
+      title: "Software Engineer",
+      company: "Online Solutions",
+      dates: "2009-11 – 2011-01",
+      dots: [
+        "Implemented various **.NET** and **SharePoint** applications using **C#** and **Javascript**",
+      ],
+    },
+  ],
+  skills: [
+    "**Languages & Frameworks**: JavaScript, Node.js, React.js, TypeScript, GraphQL, Next.js, Redux, TailwindCSS, MaterialUI, Ant Design",
+    "**Databases**: MySQL, PostgreSQL, MSSQL, MongoDB",
+    "**Version Control**: Git, GitHub, GitLab",
+    "**DevOps & CI/CD**: Docker, Kubernetes, Jenkins, GitHub Actions",
+    "**Testing**: Jest, Mocha, Cypress, Playwright",
+  ],
+  education: [
+    {
+      title: "Computer Science",
+      company: "Uniwersytet Wrocławski",
+      dates: "2007-10 - 2014-10, unfinished",
+    },
+  ],
+  languages: ["Polish", "English"],
+  hobbies: ["VR Games", "Cooking", "Lego"],
+};
